93ca5cad55af833ba89e07c9b5cf3ab25161e028
tresorsawasawa <tresorsawasawa@gmail.com>
2022-01-18 21:34:46 +0200

    Add the solution of the srtingLength challenge

---
+++ index.js
@@ -0,0 +1,8 @@
+const stringLength = (string) => {
+  if (string.length < 1 || string.length > 10)
+    throw new Error ('String length is expected to be in the range of 1 - 10');
+  return string.length;
+}
+
+
+module.exports = stringLength 
---
+++ index.test.js
@@ -0,0 +1,15 @@
+const { it } = require('eslint/lib/rule-tester/rule-tester');
+const { string } = require('stylelint/lib/formatters');
+const stringLength = require ('./index.js');
+
+it('String length', () => {
+  expect(stringLength('hello')).toBe(5);
+});
+
+it('String length out the range', () => {
+  expect(() => stringLength('hello microverse'))
+    .toThrow('String length is expected to be in the range of 1 - 10');
+  expect(() =>  stringLength(''))
+    .toThrow('String length is expected to be in the range of 1 - 10');
+});
+
